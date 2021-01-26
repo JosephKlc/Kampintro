10793430fad8a3f90a4b92fd7307f554dea6b119
yusuf kılıç <ysf_klc95@hotmail.com>
2021-01-26 23:27:20 +0300

    23 Ocak Ders Sonu

---
+++ Oop2/CustomerManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oop2
+{
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            //if (musteri.)
+            //{
+
+            //}
+
+
+
+
+
+        }
+
+    }
+}
